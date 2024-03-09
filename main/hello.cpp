9c799e3701443de46f601157d9cce7fbe648b149
Vaibhav Isloor <viisloor@gmail.com>
2024-03-09 11:47:54 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "My SRN is PES2UG21CS584";
+  cout << "My section is J section";
   return 0;
 }
